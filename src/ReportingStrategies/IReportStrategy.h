1d689507eca4ef8f3bfb05aef83ee8fab88b758a
khanhn <knultimate@live.com>
2014-04-28 15:18:13 -0700

    Added reporting strategy

---
+++ src/ReportingStrategies/IReportStrategy.h
@@ -0,0 +1,19 @@
+/*
+ * BaseReportStrategy.h
+ *
+ *  Created on: Apr 28, 2014
+ *      Author: khanhn
+ */
+
+#ifndef BASEREPORTSTRATEGY_H_
+#define BASEREPORTSTRATEGY_H_
+
+class IReportStrategy
+{
+public:
+	virtual ~IReportStrategy();
+
+};
+
+
+#endif /* BASEREPORTSTRATEGY_H_ */
