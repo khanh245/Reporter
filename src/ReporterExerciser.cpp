152fe09c7926f4c3b6337e0f8fe9cbdbd613aa95
khanhn <knultimate@live.com>
2014-04-28 09:46:39 -0700

    Initial commit - src/ReporterExerciser.cpp

---
+++ src/ReporterExerciser.cpp
@@ -0,0 +1,15 @@
+/*
+ * ReporterExerciser.cpp
+ *
+ *  Created on: Apr 28, 2014
+ *      Author: khanhn
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Using reporter..." << endl;
+	return 0;
+}
